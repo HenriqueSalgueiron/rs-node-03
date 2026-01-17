cc478c6064a15a6f4a46c7a6831fc41fcfa4a99d
Henrique <henriquehssjc@gmail.com>
2026-01-17 18:49:31 -0300

    chore: update dependencies and make their version static, and add environment variable handling

---
+++ src/env/index.ts
@@ -0,0 +1,23 @@
+// esse arquivo:
+//  carrega as variáveis de ambiente do arquivo .env para process.env
+//  valida as variáveis de ambiente usando zod
+
+import "dotenv/config"; // só de importar ele já carrega as variáveis de ambiente
+import { z } from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+  PORT: z.coerce.number().default(3333),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  console.error(
+    "❌ Invalid environment variables:",
+    z.treeifyError(_env.error),
+  );
+  throw new Error("Invalid environment variables.");
+}
+
+export const env = _env.data;

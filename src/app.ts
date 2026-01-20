c2759964039061a3c48cab6d42b99c706d2be0a1
Henrique <henriquehssjc@gmail.com>
2026-01-20 16:14:09 -0300

    feat: add and init/integrate Prisma + feat: add PostgreSQL

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 import fastify from "fastify";
+import "dotenv/config";
+import { PrismaClient } from "generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+
+const prisma = new PrismaClient({ adapter });
 export const app = fastify();
+
+export { prisma };

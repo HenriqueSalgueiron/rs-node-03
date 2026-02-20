54d91601bdabb3a98371567a00ff95695cd304f0
Henrique <henriquehssjc@gmail.com>
2026-02-20 16:42:38 -0300

    feat: implement validate check-in use case with in-memory repository logic

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -5,4 +5,6 @@ export interface ICheckinsRepository {
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
   countByUserId(userId: string): Promise<number>;
+  findById(id: string): Promise<CheckIn | null>;
+  save(checkIn: CheckIn): Promise<CheckIn>;
 }
--- src/repositories/in-memory/in-memory-checkins-repository.ts
+++ src/repositories/in-memory/in-memory-checkins-repository.ts
@@ -16,6 +16,16 @@ export class InMemoryCheckinsRepository implements ICheckinsRepository {
     return this.items.filter((checkIn) => checkIn.user_id === userId).length;
   }
 
+  async findById(id: string) {
+    const checkIn = this.items.find((item) => item.id === id);
+
+    if (!checkIn) {
+      return null;
+    }
+
+    return checkIn;
+  }
+
   async findByUserIdOnDate(userId: string, date: Date) {
     const startOfDay = dayjs(date).startOf("date");
     const endOfdate = dayjs(date).endOf("date");
@@ -42,6 +52,16 @@ export class InMemoryCheckinsRepository implements ICheckinsRepository {
     };
 
     this.items.push(checkIn);
+
+    return checkIn;
+  }
+
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id);
+
+    if (checkInIndex >= 0) {
+      this.items[checkInIndex] = checkIn;
+    }
 
     return checkIn;
   }

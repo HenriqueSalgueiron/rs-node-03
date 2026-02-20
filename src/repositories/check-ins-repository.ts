d7ee9c0ff6622f12ee6793d58fabba7bb6e5ea8f
Henrique <henriquehssjc@gmail.com>
2026-02-20 15:37:53 -0300

    feat: implement fetch user check-ins history use case with pagination and tests

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -2,5 +2,6 @@ import { CheckIn, Prisma } from "generated/prisma/client";
 
 export interface ICheckinsRepository {
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
+  findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
 }
--- src/repositories/in-memory/in-memory-checkins-repository.ts
+++ src/repositories/in-memory/in-memory-checkins-repository.ts
@@ -6,6 +6,12 @@ import dayjs from "dayjs";
 export class InMemoryCheckinsRepository implements ICheckinsRepository {
   public items: CheckIn[] = [];
 
+  async findManyByUserId(userId: string, page: number = 1) {
+    return this.items
+      .filter((checkIn) => checkIn.user_id === userId)
+      .slice((page - 1) * 20, page * 20);
+  }
+
   async findByUserIdOnDate(userId: string, date: Date) {
     const startOfDay = dayjs(date).startOf("date");
     const endOfdate = dayjs(date).endOf("date");
